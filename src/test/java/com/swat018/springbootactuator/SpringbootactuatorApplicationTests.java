92d80f65cf7b7fe51ba8f354e4710a38ec70d6a0
jinwoo park <swat018@gmail.com>
2020-06-13 23:42:07 +0900

    Initial commit

---
+++ src/test/java/com/swat018/springbootactuator/SpringbootactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootactuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
