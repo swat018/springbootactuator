71d93b91f957496ffe516df5c153133bace4b7aa
jinwoo park <swat018@gmail.com>
2020-06-14 01:23:18 +0900

    스프링부트 Actuator

--- src/main/java/com/swat018/springbootactuator/SpringbootactuatorApplication.java
+++ src/main/java/com/swat018/springbootactuator/SpringbootactuatorApplication.java
@@ -2,9 +2,15 @@ package com.swat018.springbootactuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootactuatorApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootactuatorApplication.class, args);
